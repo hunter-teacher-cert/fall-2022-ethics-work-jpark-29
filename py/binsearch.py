6c29796821e85aa66e5f759ae7d9b65cb447abc7
jpark-29 <100715306+jpark-29@users.noreply.github.com>
2022-09-27 17:23:56 +0000

    binSearch and Cgol Updates

--- py/binsearch.py
+++ py/binsearch.py
@@ -3,3 +3,41 @@
 # CSCI 77800 Fall 2022
 # collaborators: 
 # consulted: 
+
+
+# This algorithm only works on sorted ArrayLists.
+
+data = [0,1,3,7,9,10]
+
+def binarySearch(data, value):
+
+# create assign variables representing the high, low and middle indices 
+# while we're not done:
+#   if the item is at middle, return middle
+#   otherwise, update high, low, and middle
+  low = 0
+  high = len(data) - 1
+  mid = int((low + high)/2) # average
+
+  #Print out the values of lo:mid:hi for each call
+  #print(low + ":" + mid + ":" + high);
+
+  binaryS = True
+  
+  while(binaryS):
+    if(data[mid] == value):
+      return mid
+    else: # when the data != value
+      if(value < data[mid]): # when value < mid
+        high = mid - 1
+        mid = int((low + high)/2)
+      else: #when value > mid
+        low = mid + 1
+        mid = int((low + high)/2)
+      if(low > high): #this was a breakthrough!!! 
+        binaryS = False
+  return -1 #when the value is not in data 
+
+
+
+print("The number's index in the list is ", binarySearch(data,3))
--- py/gol.py
+++ py/gol.py
@@ -4,53 +4,49 @@
 # collaborators: 
 # consulted: 
 
-#create, initialize, and return  empty board (all cells dead) 
-def createNewBoard(rows, cols):
 
-  board = new char[rows][cols]                                                       
-  for i in rows: 
-    for j in cols: 
-      board[i][j]='-'
-    
+#create, initialize, and return  empty board (all cells '-') 
+def createNewBoard(numRows, numCols):
+  board = []
+                
+  for i in range(numRows): 
+    row = []
+    for j in range(numCols): 
+      row.append('-')
+
+    board.append(row)
   
   return board
-
 
 
 #print the board to the terminal 
 def printBoard(board):
 
-  for ( i = 0 i < board.length i++) 
+  for rows in range(len(board)):
+    for cols in range(len(board[rows])):
+      print(board[rows][cols], end = " ")  
+    print()
   
-    for ( j = 0 j < board[i].length j++) #i instead of row??
-    
-      System.out.print(board[i][j] + " ") 
-    
-    print("")
-  
-
-
 
 #set cell (r,c) to val 
 
 #make the new random population
-#  deadOrAliveTest = random.next(10) +1
+#  '-'Or'X'Test = random.next(10) +1
 def setCell(board, r, c, val):
 
-  board [r][c] = val
-
+  board[r][c] = val
 
 
 #return number of living neigbours of board[r][c] 
 def countNeighbours(board, r,  c):
 
-   count = 0
-   rows = board.length
-   cols = board[0].length
+  count = 0
+  rows = len(board)
+  cols = len(board[0])
   
   #iterate through the nine neighboring cells 
-  for ( i = r-1 i <= r+1 i++) 
-    for ( j = c-1 j <= c+1 j++) 
+  for i in range(r - 1, r + 2, 1):
+    for j in range(c - 1, c + 2, 1): 
       #if the current cell is the targeted cell, then skip the code below and continue with the incremented value in the above for loop
       if (i == r and j == c):
         continue
@@ -58,11 +54,8 @@ def countNeighbours(board, r,  c):
       #i >= 0 is to check if the left top corner is not out of the bound
       if ((i >= 0 and i < rows) and (j >= 0 and j < cols)):
           if (board[i][j] == 'X'):
-            count ++
-           
-         
-    
-  
+            count += 1
+
   return count
 
 
@@ -72,21 +65,21 @@ def countNeighbours(board, r,  c):
 #
 #   printecond: given a board and a cell
 #   postcond: return next generation cell state based on #CGOL rules
-#  (alive 'X', dead '-')
+#  ('X' 'X', '-' '-')
 #
 
-#check the conditions for live/dead
+#check the conditions for live/'-'
 #a live cell with fewer than 2 live neighbors dies, as if by underpopulation
 #a live cell with more than 3 live neighbors dies, as if by overpopulation
-#a dead cell with exactly 3 live neighbors becomes a live cell, as if by reprintoduction
-def getNextGenCell(  board, r,  c ):
+#a '-' cell with exactly 3 live neighbors becomes a live cell, as if by reprintoduction
+def getNextGenCell(board, r,  c ):
 
   #initialize the number of live neighbors 
-   numAlive = countNeighbours(board, r, c)
+  numAlive = countNeighbours(board, r, c)
   #a live cell with fewer than 2  or more than 3 live neighbors dies
   if (board[r][c] == 'X' and (numAlive < 2 or numAlive > 3)):
     return '-'
-   #a dead cell with exactly 3 live neighbors becomes a live cell 
+   #a '-' cell with exactly 3 live neighbors becomes a live cell 
   elif (board[r][c] == '-' and numAlive == 3):
     return 'X'    
   
@@ -100,28 +93,25 @@ def generateNextBoard(board):
   #create a new variable to hold a new temporary board
   #iterate through the entire original board and run the getNextGenCell() on it and store the result in the new temporary board
   #return the new temporary board
-   tempBoard = createNewBoard(board.length, board[0].length)   
-  for( i = 0 i < board.length i++)
-    for( j = 0 j < board[0].length j++)
+  tempBoard = createNewBoard(len(board), len(board[0]))   
+  for i in range(len(board)):
+    for j in range(len(board[0])):
       tempBoard[i][j] = getNextGenCell(board, i, j)
     
-  
   return tempBoard
 
 
-
+#main function 
 def main():
 
-  
-   board
   board = createNewBoard(10,10)
-   gen = 1
+  gen = 1
   #breathe life o some cells:
   setCell(board, 1, 1, 'X')
   setCell(board, 2, 1, 'X')
   setCell(board, 3, 1, 'X')
   
-  print("Gen " + gen + ":")
+  print("Gen", gen, ":")
   printBoard(board)
   print("--------------------------\n\n")
   
@@ -129,33 +119,24 @@ def main():
   
   board = generateNextBoard(board)
   
-  gen ++
-  print("Gen " + gen + ":")
+  gen += 1
+  print("Gen", gen, ":")
   printBoard(board)
   print("--------------------------\n\n")
   
   board = generateNextBoard(board)
 
-  gen ++
-  print("Gen " + gen + ":")
+  gen += 1
+  print("Gen", gen, ":")
   printBoard(board)
   print("--------------------------\n\n")
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
   # TASK:
   # Once your initial version is running,
   # try out different starting configurations of living cells...
   # (Feel free to comment out the above three lines.)
 
-  print("Gen X:")
-  printBoard(board)
-  print("--------------------------\n\n")
+#call main funciton
+main()
 
-  board = generateNextBoard(board)
-
-  print("Gen X+1:")
-  printBoard(board)
-  print("--------------------------\n\n")
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-#end main()
-
-#end class
+# DONE!!
